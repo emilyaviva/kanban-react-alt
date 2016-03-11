5932681e18db6fe001252cf0488df0aba0e6fe65
Emily Aviva Kapor-Mater <emily@emilyaviva.com>
2016-03-11 10:10:50 -0800

    basic Alt implementation (5)

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,68 +1,44 @@
-import uuid from 'node-uuid'
 import React from 'react'
 import Notes from './Notes'
+import NoteActions from '../actions/NoteActions'
+import NoteStore from '../stores/NoteStore'
 
 class App extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      notes: [
-        {
-          id: uuid.v4(),
-          task: 'Learn Webpack'
-        }, {
-          id: uuid.v4(),
-          task: 'Learn React'
-        }, {
-          id: uuid.v4(),
-          task: 'Do laundry'
-        }
-      ]
-    }
-  }
-
-  addNote = () => {
-    this.setState({
-      notes: this.state.notes.concat([{
-        id: uuid.v4(),
-        task: 'New task'
-      }])
+  addNote() {
+    NoteActions.create({
+      task: 'New task'
     })
   }
 
-  editNote = (id, task) => {
+  editNote(id, task) {
     if (!task.trim()) {
       return
     }
-    const notes = this.state.notes.map(note => {
-      if (note.id === id && task) {
-        note.task = task
-      }
-      return note
+    NoteActions.update({
+      id,
+      task
     })
-    this.setState({notes})
   }
 
-  deleteNote = (id, e) => {
+  deleteNote(id, e) {
     e.stopPropagation()
-    this.setState({
-      notes: this.state.notes.filter(note => note.id !== id)
-    })
+    NoteActions.delete(id)
   }
 
   render() {
-    const notes = this.state.notes
     return (
       <div>
         <button className="add-note" onClick={this.addNote}>
           <i className="fa fa-plus"></i>
         </button>
-        <Notes
-          notes={notes}
-          onEdit={this.editNote}
-          onDelete={this.deleteNote}
-        />
+        <AltContainer
+          stores={[NoteStore]}
+          inject={{
+            notes: () => NoteStore.getState().notes
+          }}
+        >
+          <Notes onEdit={this.editNote} onDelete={this.deleteNote} />
+        </AltContainer>
       </div>
     )
   }
--- app/index.jsx
+++ app/index.jsx
@@ -1,8 +1,14 @@
+import alt from './libs/alt'
+import storage from './libs/storage'
+import persist from './libs/persist'
+
 import 'font-awesome/css/font-awesome.css'
 import './main.scss'
 
 import React from 'react'
 import { render } from 'react-dom'
 import App from './components/App.jsx'
+
+persist(alt, storage, 'app')
 
 render(<App />, document.getElementById('app'))
